613caa74aa0c4432fb033a16231fd3f8973efcd5
shashank-sde <115834509+shashank-sde@users.noreply.github.com>
2022-12-10 01:47:19 -0500

    Add files via upload

---
+++ static_web_page/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar.js';
+import BlogBody from './components/BlogBody/BlogBody.js';
+import Footer from './components/Footer/Footer.js';
+import BlogPage from './components/BlogPage/BlogPage.js';
+import BlogPage3 from './components/BlogPage/BlogPage3.js';
+import BlogPage2 from './components/BlogPage/BlogPage2.js';
+import Apirefresh from './components/Apirefresh/Apirefresh.js';
+import Sample from './components/Sample/Sample.js';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Routes,
+  Link,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<BlogBody/>} />
+      <Route path="/post1" element={<BlogPage/>} /> 
+      <Route path="/post2" element={<BlogPage2/>} />
+      <Route path="/post3" element={<BlogPage3/>} />
+      <Route path="/apirefresh" element={<Apirefresh/>}/>
+    </Routes>    
+    <Footer/>  
+    
+    </>
+  );
+}
+
+export default App;
