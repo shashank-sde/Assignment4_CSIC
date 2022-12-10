233a580b62d675d4a646be36438795911c5ec9ef
shashank-sde <115834509+shashank-sde@users.noreply.github.com>
2022-12-10 01:55:12 -0500

    Add files via upload

---
+++ static_web_page/src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div class="header">
+      <h1>The BlogCase</h1>
+      <p class="desc">Your one stop destination to glance your favourite blogs.</p>
+    </div>
+    
+  )
+}
+
+export default Navbar
