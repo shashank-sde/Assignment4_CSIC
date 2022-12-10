9737b8d458827ffab015a61e9a7010ee73aeb811
shashank-sde <115834509+shashank-sde@users.noreply.github.com>
2022-12-10 01:54:35 -0500

    Add files via upload

---
+++ static_web_page/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div class="footer">
+            <p>Copyright 2002 - 2032</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
